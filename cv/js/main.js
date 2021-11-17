d7b42cd032301344501a6303d3f1ee918f5a2404
DonSergio91 <serjuck@gmail.com>
2021-11-17 23:13:25 +0200

    cv commit

---
+++ cv/js/main.js
@@ -0,0 +1,8 @@
+$('.portfolio__inner').slick({
+    slidesToShow: 1,
+    arrows: true,
+    fade: true,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="./images/icons/left.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="./images/icons/right.svg" alt=""></button>',
+    slidesToScroll: 1,
+});
